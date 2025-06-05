c7540d59fd902e263ebd665b51f98f4ea189fa2b
anonimo2020 <69107898+anonimo2020@users.noreply.github.com>
2025-06-04 23:12:19 -0500

    main.py

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ def handle_disconnect():
 # Main
 # ====================
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+    socketio.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
