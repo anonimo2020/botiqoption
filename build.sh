0c77a47c6968570d9275f4a004c527fb83c03d88
anonimo2020 <69107898+anonimo2020@users.noreply.github.com>
2025-06-08 18:58:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -120,7 +120,7 @@ requests==2.31.0
 gunicorn==21.2.0
 python-dateutil==2.8.2
 pytz==2023.3
-websocket-client==1.6.4
+websocket-client==1.1.0
 certifi==2023.7.22
 urllib3==2.0.7
 git+https://github.com/Lu-Yi-Hsun/iqoptionapi.git
