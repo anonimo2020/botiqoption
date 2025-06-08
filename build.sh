77be01efa0430ce901ee55732cb8431fb69f729c
anonimo2020 <69107898+anonimo2020@users.noreply.github.com>
2025-06-08 10:36:51 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ echo "=== Starting build process ==="
 # Instala dependencias desde requirements.txt
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install git+https://github.com/Lu-Yi-Hsun/iqoptionapi.git
 
 echo "✅ Build completo. Usando iqoptionapi local (no se instalará desde GitHub)."
