7d74bde75b9fe8a1dbbf8d93a6fa83c48ee2a17d
anonimo2020 <69107898+anonimo2020@users.noreply.github.com>
2025-06-10 23:23:53 -0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,10 @@
 import os
 
 bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
-workers = 1  # Importante: Solo 1 worker para SocketIO
+workers = 1
 worker_class = "eventlet"
 worker_connections = 1000
 timeout = 120
 keepalive = 2
 max_requests = 1000
-max_requests_jitter = 50
 preload_app = True
